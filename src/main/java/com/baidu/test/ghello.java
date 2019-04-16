f4c40b7587f7bc15973c7f569f4fc979bbe36b7f
liuzhitog <1441812666@qq.com>
2019-04-16 20:35:50 +0800

    初始化

---
+++ src/main/java/com/baidu/test/ghello.java
@@ -0,0 +1,9 @@
+package com.baidu.test;
+
+//（一）使用Maven创建Java项目。（10分）
+//（二）编写main方法，输出helloWord。（10分）
+public class ghello {
+  public static void main(String[] args) {
+	System.out.println("hello11");
+}
+}
